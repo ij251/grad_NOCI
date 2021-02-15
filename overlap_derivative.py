deffd4f174d59e4e03d2d98bad8447feb742e64b
ij251 <ij251@cam.ac.uk>
2021-02-15 17:28:55 +0000

    Altered functions to support the complex symmetric case (holomorphic hartree fock states)

--- overlap_derivative.py
+++ overlap_derivative.py
@@ -3,12 +3,13 @@ from first_order_ghf import *
 from zeroth_order_ghf import *
 
 
-# g1 = g1_iteration(False, mol, atom, coord, nelec)
-
-def get_wxlambda0(gw0, gx0, mol):
+def get_wxlambda0(gw0, gx0, mol, complexsymmetric: bool):
 
     sao0 = mol.intor("int1e_ovlp")
-    wxs0 = np.linalg.multi_dot([gw0.T.conj(), sao0, gx0])
+    if not complexsymmetric:
+        wxs0 = np.linalg.multi_dot([gw0.T.conj(), sao0, gx0])
+    else:
+        wxs0 = np.linalg.multi_dot([gw0.T, sao0, gx0])
 
     wxu,_,wxvh = np.linalg.svd(wxs0)
     wxv = wxvh.T.conj()
@@ -18,15 +19,22 @@ def get_wxlambda0(gw0, gx0, mol):
     return wxlambda0
 
 
-def get_wxlambda1(gw0, gw1, gx0, gx1, mol, atom, coord):
+def get_wxlambda1(gw0, gw1, gx0, gx1, mol, atom, coord,
+                  complexsymmetric: bool):
 
     sao0 = mol.intor("int1e_ovlp")
     sao1 = get_s1(mol, atom, coord)
 
-    wxs0 = np.linalg.multi_dot([gw0.T.conj(), sao0, gx0])
-    wxs1 = (np.linalg.multi_dot([gw1.T.conj(), sao0, gx0])
-            + np.linalg.multi_dot([gw0.T.conj(), sao1, gx0])
-            + np.linalg.multi_dot([gw0.T.conj(), sao0, gx1]))
+    if not complexsymmetric:
+        wxs0 = np.linalg.multi_dot([gw0.T.conj(), sao0, gx0])
+        wxs1 = (np.linalg.multi_dot([gw1.T.conj(), sao0, gx0])
+                + np.linalg.multi_dot([gw0.T.conj(), sao1, gx0])
+                + np.linalg.multi_dot([gw0.T.conj(), sao0, gx1]))
+    else:
+        wxs0 = np.linalg.multi_dot([gw0.T, sao0, gx0])
+        wxs1 = (np.linalg.multi_dot([gw1.T, sao0, gx0])
+                + np.linalg.multi_dot([gw0.T, sao1, gx0])
+                + np.linalg.multi_dot([gw0.T, sao0, gx1]))
 
     wxu,_,wxvh = np.linalg.svd(wxs0)
     wxv = wxvh.T.conj()
@@ -60,33 +68,44 @@ def get_g1_list(g0_list, complexsymmetric: bool, mol, atom, coord, nelec):
 
     return g1_list
 
-def get_s1mat(nnoci, g0_list, g1_list, mol, atom, coord, nelec):
+
+def get_s1mat(nnoci, g0_list, g1_list, mol, atom, coord, nelec,
+              complexsymmetric: bool):
 
     s1mat = np.zeros(nnoci)
 
     for w in range(nnoci):
         for x in range(nnoci):
 
-            wxlambda0 = get_wxlambda0(g0_list[w], g0_list[x], mol)
+            wxlambda0 = get_wxlambda0(g0_list[w], g0_list[x], mol,
+                                      complexsymmetric)
             wxlambda1 = get_wxlambda1(g0_list[w], g1_list[w], g0_list[x],
-                                      g1_list[x], mol, atom, coord)
+                                      g1_list[x], mol, atom, coord,
+                                      complexsymmetric)
 
             s1mat[w,x] = get_swx1(wxlambda0, wxlambda1, nelec)
 
     return s1mat
 
 
-def transform_g(gw0, gx0, mol):
+def transform_g(gw0, gx0, mol, complexsymmetric: bool):
 
     sao0 = mol.intor("int1e_ovlp")
-    wxs0 = np.linalg.multi_dot([gw0.T.conj(), sao0, gx0])
+    if not complexsymmetric:
+        wxs0 = np.linalg.multi_dot([gw0.T.conj(), sao0, gx0])
+    else:
+        wxs0 = np.linalg.multi_dot([gw0.T, sao0, gx0])
 
     wxu,_,wxvh = np.linalg.svd(wxs0)
     wxv = wxvh.T.conj()
     det_wxu = np.linalg.det(wxu)
     det_wxv = np.linalg.det(wxv)
 
-    gw0_t = np.dot(gw0, wxu)
+    if not complexsymmetric:
+        gw0_t = np.dot(gw0, wxu)
+    else:
+        gw0_t = np.dot(gw0, wxu.conj())
+
     gx0_t = np.dot(gx0, wxv)
 
     gw0_t[:,0] *= 1/det_wxu
