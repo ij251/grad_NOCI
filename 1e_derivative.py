b7243cab1442f364851234c667726966d2864672
ij251 <ij251@cam.ac.uk>
2021-02-18 09:52:28 +0000

    Wrote function to calculate 1e hamiltonian derivative

--- 1e_derivative.py
+++ 1e_derivative.py
@@ -3,7 +3,8 @@ from first_order_ghf import *
 from zeroth_order_ghf import *
 from overlap_derivative import *
 
-def get_xwp0(gw0_t, gx0_t, orbital_num, complexsymmetric):
+
+def get_xwp0(gw0_t, gx0_t, orbital_num, complexsymmetric: bool):
 
     if not complexsymmetric:
         xwp0 = np.dot(gx0_t[:, orbital_num], gw0_t[:, orbital_num].T.conj())
@@ -13,7 +14,7 @@ def get_xwp0(gw0_t, gx0_t, orbital_num, complexsymmetric):
 return xwp0
 
 
-def get_xwp1(gw0_t, gx0_t, gw1_t, gx1_t, orbital_num, complexsymmetric):
+def get_xwp1(gw0_t, gx0_t, gw1_t, gx1_t, orbital_num, complexsymmetric: bool):
 
     if not complexsymmetric:
         xwp1 = (np.dot(gx0_t[:, orbital_num],gw1_t[:, orbital_num].T.conj())
@@ -25,21 +26,35 @@ def get_xwp1(gw0_t, gx0_t, gw1_t, gx1_t, orbital_num, complexsymmetric):
 return xwp1
 
 
-def get_onewx1(mol, atom, coord, gw0, gx0, gw1, gx1, nelec, complexsymmetric):
+def get_onewx1(mol, atom, coord, gw0, gx0, gw1, gx1, nelec,
+               complexsymmetric: bool):
 
     hcore0 = get_hcore0(mol)
     hcore1 = get_hcore1(mol, atom, coord)
     wxlambda0 = get_wxlambda0(gw0, gx0, mol, complexsymmetric)
     wxlambda1 = get_wxlambda1(gw0, gw1, gx0, gx1, mol, atom, coord,
                               complexsymmetric)
+    wxlambda1_diag = np.diag(wxlambda1)
     gw0_t, gx0_t = transform_g(gw0, gx0, mol, complexsymmetric)
 
-
+    onewx1 = 0
     for m in range(nelec):
 
-        lowdin_prod = lowdin_prod(wxlambda0, m)
-        xwp0 = get_xwp0(gw0, 
+        lowdin_prod0 = lowdin_prod(wxlambda0, m)
+        lowdin_prod1 = (np.sum(wxlambda1_diag[j,j]*lowdin_prod(wxlambda0,
+                                                               [j,m])
+                              for j in range(nelec))
+                        - wxlambda1_diag[m,m]*lowdin_prod(wxlambda0,m))
+        xwp0 = get_xwp0(gw0_t, gx0_t, m, complexsymmetric)
+        xwp1 = get_xwp1(gw0_t, gx0_t, gw0_t, gx1_t, m, complexsymmetric)
 
+        a = lowdin_prod0 * np.einsum("ij,ji->", hcore1, xwp0)
+        b = lowdin_prod1 * np.einsum("ij,ji->", hcore0, xwp0)
+        c = lowdin_prod0 * np.einsum("ij,ji->", hcore0, xwp1)
+
+        onewx1 += a + b + c
+
+    return onexw1
 
 
 
