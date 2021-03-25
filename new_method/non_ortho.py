01c8ce1f6506d14ca926db16dfd129da264485d6
ij251 <ij251@cam.ac.uk>
2021-03-25 17:17:54 +0000

    Edited docstrings and variable/function names to fit better with new theory

--- new_method/non_ortho.py
+++ new_method/non_ortho.py
@@ -6,7 +6,7 @@ from zeroth_order_ghf import *
 
 def lowdin_pairing(w_g, x_g, sao = None, mol, nelec, complexsymmetric: bool):
 
-    r"""Calculates the order diagonal matrix of singular values,
+    r"""Calculates the diagonal matrix of singular values,
     calculated from a singular value decomposition of the molecular orbital
     overlap matrix between determinants :math:'\prescript{w}{}{\Psi}' and
     :math:'\prescript{x}{}{\Psi}'. This is the Löwdin tranformation required
@@ -98,8 +98,29 @@ def lowdin_pairing(w_g, x_g, sao = None, mol, nelec, complexsymmetric: bool):
 
 def get_sao1_partial(mol, atom, coord):
 
-    r"""Function gives parts of the sao derivative matrix needed to find the
-    Lowdin overlaps in terms B and D of the matrix element derivative.
+    r"""Function gives the overlap tensors where the bra or the ket contains
+    differentiated AO basis functions, as needed to calculate terms B and D of
+    the first order overlap between determinants in the theory.
+
+    .. math::
+
+            \left(\mathbf{S}_{\mathrm{bra}}^{(1)}\right)_{\mu\nu}
+            = \left(\phi^{(1)}_{\cdot \mu}\Big|\phi^{(0)}_{\cdot \nu}\right)\\
+
+            \left(\mathbf{S}_{\mathrm{ket}}^{(1)}\right)_{\mu\nu}
+            = \left(\phi^{(0)}_{\cdot \mu}\Big|\phi^{(1)}_{\cdot \nu}\right)
+
+    :param mol: Molecule class as defined by PySCF.
+    :param atom: Input for which atom is being perturbed, with atoms numbered
+            according to the PySCF molecule.
+    :param coord: Input for along which coordinate the pertubation of the atom
+            lies.
+            coord = '0' for x
+                    '1' for y
+                    '2' for z
+
+    :returns: Two matrices, the first of which has the bra differentiated and
+            the second the ket.
     """
 
     sao0 =  mol.intor("int1e_ovlp")
@@ -127,7 +148,98 @@ def get_sao1_partial(mol, atom, coord):
     return s1_bra, s1_ket
 
 
-def lowdin_prod(wxlambda0, rmind):
+def get_j1_partial(mol, atom coord):
+
+    r"""Function gets the 4th order two electron integral tensors where one
+    of each of the 4 positions (bra0,bra1|ket0,ket1) contains differentiated
+    AO basis functions, as needed to calculate terms B and D of the first
+    order two electron contribution to the hamiltonian matrix element.
+
+    .. math::
+
+            \left(\mathbf{j}_{\mathrm{bra}_0}^{(1)}\right)_{\mu\mu'\nu\nu'}
+            = \left(\phi^{(1)}_{\mu}\phi^{(0)}_{\mu'}|
+              \phi^{(0)}_{\nu}\phi^{(0)}_{\nu'}\right)\\
+
+            \left(\mathbf{j}_{\mathrm{bra}_1}^{(1)}\right)_{\mu\mu'\nu\nu'}
+            = \left(\phi^{(0)}_{\mu}\phi^{(1)}_{\mu'}|
+              \phi^{(0)}_{\nu}\phi^{(0)}_{\nu'}\right)
+
+             \left(\mathbf{j}_{\mathrm{ket}_0}^{(1)}\right)_{\mu\mu'\nu\nu'}
+             = \left(\phi^{(0)}_{\mu}\phi^{(0)}_{\mu'}|
+               \phi^{(1)}_{\nu}\phi^{(0)}_{\nu'}\right)
+
+             \left(\mathbf{j}_{\mathrm{ket}_1}^{(1)}\right)_{\mu\mu'\nu\nu'}
+             = \left(\phi^{(0)}_{\mu}\phi^{(0)}_{\mu'}|
+               \phi^{(0)}_{\nu}\phi^{(1)}_{\nu'}\right)
+
+    :param mol: Molecule class as defined by PySCF.
+    :param atom: Input for which atom is being perturbed, with atoms numbered
+            according to the PySCF molecule.
+    :param coord: Input for along which coordinate the pertubation of the atom
+            lies.
+            coord = '0' for x
+                    '1' for y
+                    '2' for z
+
+    :returns: 4 tensors, the first of which has bra0 differentiated, the
+            second bra1, the third ket0 and the fourth ket1.
+    """
+
+    omega = np.identity(2)
+    spin_j = np.einsum("ij,kl->ikjl", omega, omega)
+
+    twoe = -mol.intor("int2e_ip1")[coord] #minus sign due to pyscf definition
+
+    j1_bra0 = np.zeros_like(twoe)
+    j1_bra1 = np.zeros_like(twoe)
+    j1_ket0 = np.zeros_like(twoe)
+    j1_ket1 = np.zeros_like(twoe)
+
+    for i in range(twoe.shape[0]):
+
+        atoms_i = int(i in range(mol.aoslice_by_atom()[atom][2],
+                                  mol.aoslice_by_atom()[atom][3]))
+
+        for j in range(twoe.shape[0]):
+
+            atoms_j = int(j in range(mol.aoslice_by_atom()[atom][2],
+                                      mol.aoslice_by_atom()[atom][3]))
+
+            for k in range(twoe.shape[0]):
+
+                atoms_k = int(k in range(mol.aoslice_by_atom()[atom][2],
+                                          mol.aoslice_by_atom()[atom][3]))
+
+                for l in range(twoe.shape[0]):
+
+                    atoms_l = int(l in range(mol.aoslice_by_atom()[atom][2],
+                                              mol.aoslice_by_atom()[atom][3]))
+
+                    j1_bra0[i][j][k][l] += twoe[i][j][k][l] * atoms_i
+                    j1_bra1[i][j][k][l] += twoe[j][i][k][l] * atoms_j
+                    j1_ket0[i][j][k][l] += twoe[k][l][i][j] * atoms_k
+                    j1_ket1[i][j][k][l] += twoe[l][k][i][j] * atoms_l
+
+
+    j1_bra0 = np.einsum("abcd->acbd", j1_bra0,
+                        optimize='optimal') #convert to physicists
+    j1_bra1 = np.einsum("abcd->acbd", j1_bra1,
+                        optimize='optimal') #convert to physicists
+    j1_ket0 = np.einsum("abcd->acbd", j1_ket0,
+                        optimize='optimal') #convert to physicists
+    j1_ket1 = np.einsum("abcd->acbd", j1_ket1,
+                        optimize='optimal') #convert to physicists
+
+    j1_bra0 = np.kron(spin_j, j1_bra0)
+    j1_bra1 = np.kron(spin_j, j1_bra1)
+    j1_ket0 = np.kron(spin_j, j1_ket0)
+    j1_ket1 = np.kron(spin_j, j1_ket1)
+
+    return j1_bra0, j1_bra1, j1_ket0, j1_ket1
+
+
+def lowdin_prod(wxlambda, rmind):
 
     r"""Calculates product of Löwdin singular values with option to remove
     certain values, for use in calculating overlap derivatives. For example,
@@ -137,15 +249,16 @@ def lowdin_prod(wxlambda0, rmind):
 
             \prod\limits_{i\not = j}^{N_e}\prescript{wx}{}\lambda_i
 
-    :param wxlambda0: Diagonal matrix of Löwdin singular values for the wth
+    :param wxlambda: Diagonal matrix of Löwdin singular values for the wth
             and xth determinant.
     :param rmind: A python list of indices to be removed from the product
 
     :returns: A value for the singular value product
     """
-    wxlambda0_diag = np.diag(wxlambda0)
-    ind = list(set(range(len(wxlambda0_diag)))-set(rmind))
-    lowdin_prod = np.prod(wxlambda0_diag[ind])
+
+    wxlambda_diag = np.diag(wxlambda)
+    ind = list(set(range(len(wxlambda_diag)))-set(rmind))
+    lowdin_prod = np.prod(wxlambda_diag[ind])
 
     return lowdin_prod
 
@@ -179,9 +292,9 @@ def get_g1_list(mol, atom, coord, g0_list, nelec, complexsymmetric: bool):
     return g1_list
 
 
-def get_xw_p0(w_g0_t, x_g0_t, orbital_num, complexsymmetric: bool):
+def get_xw_p(w_g_t, x_g_t, orbital_num, complexsymmetric: bool):
 
-    r"""Calculates the zeroth order one particle transition density matrix
+    r"""Calculates the one particle transition density matrix
     from transformed MO coefficient matrices for determinants w and x.
 
     .. math::
@@ -190,10 +303,10 @@ def get_xw_p0(w_g0_t, x_g0_t, orbital_num, complexsymmetric: bool):
             = \prescript{w}{}{\tilde{G}}_m
             \prescript{x}{}{\tilde{G}}_m^{\dagger\diamond}
 
-    :param w_g0_t: The Löwdin transformed zeroth order molecular orbital
-            coefficient matrix of the wth determinant.
-    :param x_g0_t: The Löwdin transformed zeroth order molecular orbital
-            coefficient matrix of the xth determinant.
+    :param w_g_t: The Löwdin transformed molecular orbital coefficient matrix
+            of the wth determinant.
+    :param x_g_t: The Löwdin transformed molecular orbital coefficient matrix
+            of the xth determinant.
     :param orbital_num: The index of the particular MO.
     :param complexsymmetric: If :const:'True', :math:'/diamond = /star'.
             If :const:'False', :math:'\diamond = \hat{e}'.
@@ -202,10 +315,10 @@ def get_xw_p0(w_g0_t, x_g0_t, orbital_num, complexsymmetric: bool):
     """
 
     if not complexsymmetric:
-        xw_p0 = np.einsum("i,j->ij", x_g0_t[:, orbital_num],
-                         w_g0_t[:, orbital_num].conj())
+        xw_p = np.einsum("i,j->ij", x_g_t[:, orbital_num],
+                         w_g_t[:, orbital_num].conj())
     else:
-        xw_p0 = np.einsum("i,j->ij", x_g0_t[:, orbital_num],
-                         w_g0_t[:, orbital_num])
+        xw_p = np.einsum("i,j->ij", x_g_t[:, orbital_num],
+                         w_g_t[:, orbital_num])
 
-    return xw_p0
+    return xw_p
